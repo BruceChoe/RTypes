189ec2928c20758bf0da68c89a437ce7a7cbe9db
Elias <elias.brezine@gmail.com>
2021-12-05 00:53:56 -0800

    fixed/cleaned client/main.html, client/main.js

--- client/main.js
+++ client/main.js
@@ -8,23 +8,6 @@ import { Meteor } from 'meteor/meteor';
 
 import '/imports/api/methods';
 
-Template.hello.onCreated(function helloOnCreated() {
-    // counter starts at 0
-    this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-    counter() {
-        return Template.instance().counter.get();
-    },
-});
-
-Template.hello.events({
-    'click button'(event, instance) {
-        // increment the counter when button is clicked
-        instance.counter.set(instance.counter.get() + 1);
-    },
-});
 
 Template.fileUpload.events({
     'change input': function(ev) {
