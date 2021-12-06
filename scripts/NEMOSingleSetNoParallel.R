3786ce3fa04c5c2d6f5c2bf6d7262bf8ed5e1550
Bruce Choe <44606124+BruceChoe@users.noreply.github.com>
2021-12-05 20:30:00 -0800

    Push SNF and NEMO analysis
    Added WIP SNF and NEMO analysis scripts for Monday 12/6 presentation demo

---
+++ scripts/NEMOSingleSetNoParallel.R
@@ -0,0 +1,41 @@
+datasets = "GBM"
+dataPath="D:\\CS 425 Project\\data\\"
+resultPath="D:\\CS 425 Project\\results\\"
+
+library(NEMO)
+library(SNFtool)
+
+#load data into dataList
+load(paste(dataPath, dataset, ".RData" ,sep=""))
+patients=rownames(survival)
+patients=intersect(patients,rownames(mydatGE))
+patients=intersect(patients,rownames(mydatME))
+patients=intersect(patients,rownames(mydatMI))
+mydatGE=as.matrix(mydatGE[patients,])
+mydatME=as.matrix(mydatME[patients,])
+mydatMI=as.matrix(mydatMI[patients,])
+survival=survival[patients,]
+dataList <- list(mydatGE, mydatME, mydatMI)
+
+for (i in 1:length(dataList)) {
+  data <- dataList[[i]]
+  dataList[[i]] <- t(data)
+}
+
+#clustering = nemo.clustering(dataList) # Parameters: num.Clusters and num.neighbors
+affinity.graph = nemo.affinity.graph(dataList)
+num.clusters = nemo.num.clusters(affinity.graph)
+clustering = spectralClustering(affinity.graph, num.clusters) 
+names(clustering) = colnames(affinity.graph)
+
+#displayClusters(affinity.graph, clustering)
+
+png(paste(resultPath,"NEMO_",datasets,".png"),   
+    width     = 3.25,
+    height    = 3.25,
+    units     = "in",
+    res       = 1200,
+    pointsize = 4)
+displayClustersWithHeatmap(affinity.graph, clustering)
+dev.off()
+gc()
---
+++ scripts/SNFSingleSetNoParallel.R
@@ -0,0 +1,57 @@
+datasets = "GBM"
+dataPath="D:\\CS 425 Project\\data\\"
+resultPath="D:\\CS 425 Project\\results\\"
+
+#library(PINSPlus)
+library(SNFtool)
+
+
+#load data into dataList
+load(paste(dataPath, dataset, ".RData" ,sep=""))
+patients=rownames(survival)
+patients=intersect(patients,rownames(mydatGE))
+patients=intersect(patients,rownames(mydatME))
+patients=intersect(patients,rownames(mydatMI))
+mydatGE=as.matrix(mydatGE[patients,])
+mydatME=as.matrix(mydatME[patients,])
+mydatMI=as.matrix(mydatMI[patients,])
+survival=survival[patients,]
+
+dataList <- list(mydatGE, mydatME, mydatMI)
+
+#RUNSNF
+set.seed(1) # TODO: COMMENT THIS OUT OF PRODUCTION
+K = 20;		# number of neighbors, usually (10~30)
+alpha = 0.5;  	# hyperparameter, usually (0.3~0.8)
+NIT = 10; 	# Number of Iterations, usually (10~20)
+
+WList <- list()
+for (i in 1:length(dataList)) {
+  data <- dataList[[i]]
+  data <- standardNormalization(data)
+  PSM <- dist2(as.matrix(data), as.matrix(data));  #calculate pair-wise distance
+  W <- affinityMatrix(PSM, K, alpha)  # construct similarity graphs
+  WList[[i]] <- W
+}
+W = SNF(WList, K, NIT) # construct status matrix 
+C = estimateNumberOfClustersGivenGraph(W)[[1]] 
+group = spectralClustering(W,C) # final subtype info
+
+list(cluster = group, rt = running_time)
+result$survival <- survival
+
+#VISUALIZATION 
+print(W)
+print(group)
+#displayClustersWithHeatmap(W, group)
+
+png(paste(resultPath,"SNF_",datasets,".png"),   
+    width     = 3.25,
+    height    = 3.25,
+    units     = "in",
+    res       = 1200,
+    pointsize = 4)
+displayClustersWithHeatmap(W, group)
+plotAlluvial(W, 2:5, col="red")
+dev.off()
+gc()
