dca9772754b776952b10f61a30f7fbf7cfda0429
Erik Marsh <erik.i.marsh@gmail.com>
2022-02-17 01:48:37 -0800

    automatically add a test user to the database

--- server/main.js
+++ server/main.js
@@ -1,7 +1,14 @@
 import { Meteor } from 'meteor/meteor';
 import '/imports/api/methods';
-
+import { Users } from '/imports/api/users';
+import { Comms } from '/imports/api/comms';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  if (Users.find({}).count() === 0) {
+    Users.insert({
+      username: "test_user",
+      password: "test_password",
+      visualizations: []
+    });
+  }
 });
