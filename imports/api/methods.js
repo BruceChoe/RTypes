3e3ca05e340a1bc1e5a880731b429304b738423e
Erik Marsh <erik.i.marsh@gmail.com>
2022-02-17 01:47:37 -0800

    function that creates user directories

--- imports/api/methods.js
+++ imports/api/methods.js
@@ -1,21 +1,42 @@
 // file uploding code courtesy of https://gist.github.com/dariocravero/3922137
 
+import { Meteor } from 'meteor/meteor';
+
+import { Users } from '/imports/api/users';
+
 import childProcess from 'child_process';
+import fs from 'fs';
+import path from 'path';
+import { Comms } from './comms';
+
+const rootPath = '../../../../../';
 
 if (Meteor.isServer)
 {
     Meteor.methods({
-        //saveFile: function(blob, name, path, encoding) {
-        "saveFile"(blob, name, path, encoding) {
+        "saveFile"(user, blob, name, path, encoding) {
+            name = "users\\" + user + "\\data\\" + name;
+            Users.update(
+                { username: user },
+                {
+                    $push: {
+                        visualizations: {
+                            source_dataset: name,
+                            visualizations: []
+                        }
+                    }
+                }
+            );
+
             var path = cleanPath(path);
             var fs = Npm.require('fs');
             var name = cleanName(name || 'file');
             var encoding = encoding || 'binary';
-            var chroot = Meteor.chroot || '../../../../../public';
+            var chroot = Meteor.chroot || rootPath;
 
-            console.log(name);
-            console.log(path);
-            console.log(encoding);
+            // console.log(name);
+            // console.log(path);
+            // console.log(encoding);
 
             // Clean up the path. Remove any initial and final '/' -we prefix them-,
             // any sort of attempt to go to the parent directory '..' and any empty directories in
@@ -47,6 +68,12 @@ if (Meteor.isServer)
         "invokeProcess"(tokens) {        
             if (tokens.length === 0) return;
 
+            // Comms.insert({true: true});
+            // Meteor.publish(null, function() { return Comms.find({}); });
+            // console.log("sfsdf");
+
+            // return;
+
             let invocation = tokens[0];
             for (let i = 1; i < tokens.length; i++)
             {
@@ -54,6 +81,35 @@ if (Meteor.isServer)
             }
             
             let proc = childProcess.exec(invocation, invocationCallback);
+        },
+
+        // username: str
+        "createUserDirectories"(username) {
+            if (!username) return;
+
+            function errorCallback(err, username, subdirectrory) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(`User directory users/${username}/${subdirectrory} created successfully`);
+                }
+            }
+
+            let userPath = path.join(path.join(rootPath, 'users'));
+
+            fs.mkdir(userPath, (err) => { console.log(err); });
+
+            fs.mkdir(path.join(userPath, username), (err) => {
+                errorCallback(err, username, "");
+            });
+
+            fs.mkdir(path.join(userPath, username, 'data'), (err) => {
+                errorCallback(err, username, "data");
+            });
+
+            fs.mkdir(path.join(userPath, username, 'visualizations'), (err) => {
+                errorCallback(err, username, "visualizations");
+            });
         }
     });
 }
@@ -69,4 +125,8 @@ function invocationCallback(error, stdout, stderr) {
     }
   
     console.log(`stdout: ${stdout}`);
+}
+
+function errorCallback(error) {
+    console.log(error);
 }
