38164cf79f8ac119d4a4a7999ebe3e57bbcbdaeb
Bruce Choe <44606124+BruceChoe@users.noreply.github.com>
2022-03-12 19:31:49 -0800

    PCA test

--- scripts/visualizeTest.py
+++ scripts/visualizeTest.py
@@ -1,4 +1,6 @@
-import sklearn
+import sklearn as sk
+from sklearn.decomposition import PCA
+
 import pandas as pd
 import numpy as np
 df_status_matrix = pd.read_csv('data/W.csv')
@@ -17,7 +19,9 @@ eig_val, eigen_vec = np.linalg.eig(covariance_matrix)
 print("Eigenvector: \n",eig_val,"\n")
 print("Eigenvalues: \n", eigen_vec, "\n")
 
-
-import plotly.express as px
-fig = px.imshow(df_status_matrix)
-fig.show()
+pca = PCA().fit_transform(df_status_matrix)
+print("PCA TIMEEEE")
+print(pca)
+# import plotly.express as px
+# fig = px.imshow(df_status_matrix)
+# fig.show()
