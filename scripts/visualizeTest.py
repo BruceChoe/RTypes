b226d5a1550bee72d19f3819a636dac82b9fdb23
Bruce Choe <44606124+BruceChoe@users.noreply.github.com>
2022-03-12 19:34:24 -0800

    Update visualizeTest.py

--- scripts/visualizeTest.py
+++ scripts/visualizeTest.py
@@ -1,6 +1,8 @@
-import sklearn
-import pandas as pd
+import sklearn as sk
+from sklearn.decomposition import PCA
 
+import pandas as pd
+import numpy as np
 df_status_matrix = pd.read_csv('data/W.csv')
 df_status_matrix = df_status_matrix.set_index('Unnamed: 0')
 df_status_matrix.index.rename('Status Matrix: W', inplace=True)
@@ -12,8 +14,14 @@ df_group_matrix.index.rename('Group Matrix', inplace=True)
 print(df_group_matrix)
 
 #PCA time
-print(test)
+covariance_matrix = np.cov(df_status_matrix.T)
+eig_val, eigen_vec = np.linalg.eig(covariance_matrix)
+print("Eigenvector: \n",eig_val,"\n")
+print("Eigenvalues: \n", eigen_vec, "\n")
 
-import plotly.express as px
-fig = px.imshow(df_status_matrix)
-fig.show()
+pca = PCA().fit_transform(df_status_matrix)
+print("PCA TIMEEEE")
+print(pca)
+# import plotly.express as px
+# fig = px.imshow(df_status_matrix)
+# fig.show()
