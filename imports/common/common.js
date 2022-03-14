31b229de7723fc9cbb705facb7d5f1e1236d009b
Erik Marsh <erik.i.marsh@gmail.com>
2022-03-14 03:51:33 -0700

    templates for page elements common to all pages

---
+++ imports/common/common.js
@@ -0,0 +1,16 @@
+import { Meteor } from "meteor/meteor";
+import { EJSON } from "meteor/ejson";
+import { Template } from "meteor/templating";
+
+import { Users } from "/imports/api/users";
+import { Visualizations } from "/imports/api/visualizations";
+
+Template.sidebarVisualizationItems.helpers({
+    list() { return [...Array(10).keys()]; },
+    visualizations: () => {
+        let user = Users.find({username: "test_user"}).fetch()[0];
+        let visualizations = Visualizations.find({createdAt: { $in: user.visualizations }}).fetch();
+        console.log(visualizations);
+        return visualizations.map(v => v.createdAt);
+    }
+});
